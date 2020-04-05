3cac7269f2b9f57589561740633c419ad8f81030
anpoliakov <Andrewlybimenko@gmail.com>
2020-04-05 07:59:40 +0300

    add new entity Human

---
+++ src/main/java/models/entity/Human.java
@@ -0,0 +1,10 @@
+package models.entity;
+
+public class Human {
+    private int id;
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String password;
+    private String email;
+}
